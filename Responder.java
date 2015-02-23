e96cfc35db4c79da8a9fc3048cadfc311ed1ffce
marco <marcoiban@gmail.com>
2015-02-23 19:22:30 +0100

    0086 array list random para respuestas en la clase responder

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,20 @@
  */
 public class Responder
 {
+    private Random rnd;
+    private  ArrayList<String> a;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        this.rnd=new Random();
+        this.a= new ArrayList<>();
+        a.add("That sounds interesting. Tell me more...");
+        a.add("Tell me more...");
+        a.add("incredible i need more information");
+        a.add("it´s not good");
+        a.add("such a pity");
     }
 
     /**
@@ -20,6 +31,10 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int num= (rnd.nextInt(5));
+       String answer=(a.get(num));
+        return answer;
     }
+    
+    
 }
